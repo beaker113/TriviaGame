8df1106818a993b88aaec8673f166efacc18d201
beaker113 <43103384+beaker113@users.noreply.github.com>
2018-11-27 14:08:21 -0800

    added basic files

---
+++ Assets/CSS/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+$("#check").on("click", selectOnlyThis(this.id))
+$(".check1").on("click", selectOnlyThis(this.id));
+$(".check2").on("click", selectOnlyThis(this.id));
+$(".check3").on("click", selectOnlyThis(this.id));
+$(".check4").on("click", selectOnlyThis(this.id));
+
+
+
+
+
+
+
+
+
+
+
+});
---
+++ Assets/javascript/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $('.check').click(function() {
+        $('.check').not(this).prop('checked', false);
+    });
+    $('.check2').click(function() {
+        $('.check2').not(this).prop('checked', false);
+    });
+    $('.check3').click(function() {
+        $('.check3').not(this).prop('checked', false);
+    });
+    $('.check4').click(function() {
+        $('.check4').not(this).prop('checked', false);
+    });
+    $('.check5').click(function() {
+        $('.check5').not(this).prop('checked', false);
+    });
+    $('.check6').click(function() {
+        $('.check6').not(this).prop('checked', false);
+    });
+    $('.check7').click(function() {
+        $('.check7').not(this).prop('checked', false);
+    });
+    $('.check8').click(function() {
+        $('.check8').not(this).prop('checked', false);
+    });
+    $('.check9').click(function() {
+        $('.check9').not(this).prop('checked', false);
+    });
+
+
+});
