4a971c4f96b2b325adfafe2a679e250f65eb4630
beaker113 <43103384+beaker113@users.noreply.github.com>
2018-12-02 13:25:40 -0800

    added styling

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,31 +1,104 @@
 $(document).ready(function () {
-    $('.check').click(function() {
-        $('.check').not(this).prop('checked', false);
+    var correct = 0;
+    var incorrect = 0;
+    var t = 30;
+    var interval;
+    $("#start").on("click", function () {
+        interval = setInterval(function () {
+            t--
+            $("#time").html(t);
+            if (t === 0) {
+                //time over
+                clearInterval(interval);
+                checkWhen();
+            }
+        }, 1000);
+
     });
-    $('.check2').click(function() {
-        $('.check2').not(this).prop('checked', false);
-    });
-    $('.check3').click(function() {
-        $('.check3').not(this).prop('checked', false);
-    });
-    $('.check4').click(function() {
-        $('.check4').not(this).prop('checked', false);
-    });
-    $('.check5').click(function() {
-        $('.check5').not(this).prop('checked', false);
-    });
-    $('.check6').click(function() {
-        $('.check6').not(this).prop('checked', false);
-    });
-    $('.check7').click(function() {
-        $('.check7').not(this).prop('checked', false);
-    });
-    $('.check8').click(function() {
-        $('.check8').not(this).prop('checked', false);
-    });
-    $('.check9').click(function() {
-        $('.check9').not(this).prop('checked', false);
-    });
+
+
+
+
+
+    var checkWhen = function () {
+        var a1 = $("input[name=check1]:checked").val();
+        var a2 = $("input[name=check2]:checked").val();
+        var a3 = $("input[name=check3]:checked").val();
+        var a4 = $("input[name=check4]:checked").val();
+        var a5 = $("input[name=check5]:checked").val();
+        var a6 = $("input[name=check6]:checked").val();
+        var a7 = $("input[name=check7]:checked").val();
+        var a8 = $("input[name=check8]:checked").val();
+        var a9 = $("input[name=check9]:checked").val();
+        
+
+        if (a1 === detectiveComics) {
+            correct++;
+        }
+        else {
+            incorrect++;
+        }
+        if (a2 === mercury) {
+            correct++;
+        }
+        else {
+            incorrect++;
+        }
+        if (a3 === lego) {
+            correct++;
+        }
+        else {
+            incorrect++;
+        }
+        if (a4 === house) {
+            correct++;
+        }
+        else {
+            incorrect++;
+        }
+        if (a5 === multimedia) {
+            correct++;
+        }
+        else {
+            incorrect++;
+        }
+        if (a6 === medellin) {
+            correct++;
+        }
+        else {
+            incorrect++;
+        }
+        if (a7 === giraffe) {
+            correct++;
+        }
+        else {
+            incorrect++;
+        }
+        if (a8 === shredded) {
+            correct++;
+        }
+        else {
+            incorrect++;
+        }
+        if (a9 === right) {
+            correct++;
+        }
+        else {
+            incorrect++;
+        }
+ 
+        $("#right").html(correct);
+        $("#wrong").html(incorrect)
+    };
+
+
+
+
+    $("#submit").on("click", function () {
+        
+        checkWhen();
+    };
+
 
 
 });
