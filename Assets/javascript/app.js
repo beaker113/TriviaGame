ea7cd16c9c714d9096324efbd320cea5bd6ba6de
beaker113 <43103384+beaker113@users.noreply.github.com>
2018-12-04 18:10:17 -0800

    fixed functions

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -21,66 +21,66 @@ $(document).ready(function () {
 
 
     var checkWhen = function () {
-        var a1 = $("input[name=check1]:checked").val();
-        var a2 = $("input[name=check2]:checked").val();
-        var a3 = $("input[name=check3]:checked").val();
-        var a4 = $("input[name=check4]:checked").val();
-        var a5 = $("input[name=check5]:checked").val();
-        var a6 = $("input[name=check6]:checked").val();
-        var a7 = $("input[name=check7]:checked").val();
-        var a8 = $("input[name=check8]:checked").val();
-        var a9 = $("input[name=check9]:checked").val();
-        
+        var a1 = $("input[name='Check1']:checked").val();
+        var a2 = $("input[name='Check2']:checked").val();
+        var a3 = $("input[name='Check3']:checked").val();
+        var a4 = $("input[name='Check4']:checked").val();
+        var a5 = $("input[name='Check5']:checked").val();
+        var a6 = $("input[name='Check6']:checked").val();
+        var a7 = $("input[name='Check7']:checked").val();
+        var a8 = $("input[name='Check8']:checked").val();
+        var a9 = $("input[name='Check9']:checked").val();
+        console.log(a1);
 
-        if (a1 === detectiveComics) {
+        if (a1 === "detectiveComics") {
             correct++;
         }
         else {
             incorrect++;
         }
-        if (a2 === mercury) {
+        if (a2 === "mercury") {
             correct++;
         }
         else {
             incorrect++;
         }
-        if (a3 === lego) {
+        if (a3 === "lego") {
             correct++;
         }
         else {
             incorrect++;
         }
-        if (a4 === house) {
+        if (a4 === "house") {
             correct++;
         }
         else {
             incorrect++;
         }
-        if (a5 === multimedia) {
+        if (a5 === "multimedia") {
             correct++;
         }
         else {
             incorrect++;
         }
-        if (a6 === medellin) {
+        if (a6 === "medellin") {
             correct++;
         }
         else {
             incorrect++;
         }
-        if (a7 === giraffe) {
+        if (a7 === "giraffe") {
             correct++;
         }
         else {
             incorrect++;
         }
-        if (a8 === shredded) {
+        if (a8 === "shredded") {
             correct++;
         }
         else {
             incorrect++;
         }
-        if (a9 === right) {
+        if (a9 === "right") {
             correct++;
         }
         else {
@@ -97,7 +97,7 @@ $(document).ready(function () {
     $("#submit").on("click", function () {
         
         checkWhen();
-    };
+    });
 
 
 
